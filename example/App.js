056b80a6425baa1c6fcd55808557d1be0475546f
A.I <merrick@luois.me>
2017-05-06 14:54:16 +0800

    better example

---
+++ example/App.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+import {
+  View,
+  Text,
+  Button,
+  NativeAppEventEmitter,
+} from 'react-native'
+
+import RNPush from 'react-native-push'
+
+export default class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      registrationId: 'loading',
+    }
+  }
+
+  async componentDidMount() {
+    RNPush.initPush()
+
+    RNPush.getRegistrationId()
+      .then((id) => {
+        if (id.length) {
+          this.setState({
+            registrationId: id,
+          })
+        }
+      })
+
+    NativeAppEventEmitter.addListener('getRegistrationId', (data) => {
+      this.setState({
+        registrationId: data.registrationId,
+      })
+    })
+	}
+
+  componentWillUnmount() {
+    NativeAppEventEmitter.removeAllListeners()
+  }
+
+  handleClearPress = async () => {
+    await RNPush.clearAllNotifications()
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>registrationId: {this.state.registrationId}</Text>
+        <View>
+          <Button title="clearNotification" onPress={this.handleClearPress} />
+        </View>
+      </View>
+    )
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+}
--- example/index.android.js
+++ example/index.android.js
@@ -9,49 +9,15 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
-import RNPush from 'react-native-push'
+  View,
+} from 'react-native'
+
+import App from './App.js'
 
 export default class example extends Component {
-	async componentDidMount() {
-    RNPush.initPush()
-	}
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
+    return <App />
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('example', () => example);
+AppRegistry.registerComponent('example', () => example)
